138e7589cb95508aa140031ac266691ed12f1d80
Akash52 <ac8572611@gmail.com>
2022-09-29 17:56:43 +0530

    computed_posts

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,23 +1,37 @@
 <template>
   <div className="border-b border-gray-200">
-    <nav class="py-4 flex space-x-8 justify-center">
+    <nav class="py-4 flex flex-col space-x-8 justify-center">
+      <span class="py-4 flex space-x-8 justify-center">
+        <a
+          class="border-indigo-500 text-indigo-600 font-normal"
+          v-for="period in periods"
+          :key="period"
+          :class="{
+            'border-b-2 font-medium': period === currentPeriod,
+          }"
+          @click="setPeriod(period)"
+        >
+          {{ period }}
+        </a>
+      </span>
       <a
-        class="border-indigo-500 text-indigo-600 font-normal"
-        v-for="period in periods"
-        :key="period"
-        :class="{
-          'border-b-2 font-medium': period === currentPeriod,
-        }"
-        @click="setPeriod(period)"
+        class="px-4 my-1 border py-2 bg-pink-100 hover:bg-white cursor-pointer transition duration-500 flex"
+        v-for="post in posts"
+        :key="post.id"
       >
-        {{ period }}
+        {{ post.title }}
+        <div class="ml-2 text-slate-700 font-normal">
+          {{ post.created.format('Do MM') }}
+        </div>
       </a>
     </nav>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, computed } from 'vue';
+import moment from 'moment';
+import { today, thisWeek, thisMonth } from '../mocks';
 
 type Period = 'Today' | 'This Week' | 'This Month';
 
@@ -26,10 +40,25 @@ export default defineComponent({
   setup() {
     const periods = ['Today', 'This Week', 'This Month'];
     const currentPeriod = ref<Period>('Today');
+    const posts = computed(() => {
+      return [today, thisWeek, thisMonth].filter((post) => {
+        if (currentPeriod.value === 'Today') {
+          return post.created.isAfter(moment().subtract(1, 'day'));
+        }
+        if (currentPeriod.value === 'This Week') {
+          return post.created.isAfter(moment().subtract(1, 'week'));
+        }
+        if (currentPeriod.value === 'This Month') {
+          return post.created.isAfter(moment().subtract(1, 'month'));
+        }
+        return false;
+      });
+    });
     const setPeriod = (period: Period) => {
       currentPeriod.value = period;
     };
     return {
+      posts,
       periods,
       currentPeriod,
       setPeriod,
