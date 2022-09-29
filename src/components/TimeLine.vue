33d405d5b094a8578a9ce0849b0247ede5ca16f7
Akash52 <ac8572611@gmail.com>
2022-09-29 16:52:10 +0530

    v_for_and_composition_setup

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,15 +1,29 @@
 <template>
   <div className="border-b border-gray-200">
     <nav class="py-4 flex space-x-8 justify-center">
-      <a class="border-indigo-500 text-indigo-600"> Today </a>
-      <a class="border-indigo-500 text-indigo-600"> This Week </a>
-      <a class="border-indigo-500 text-indigo-600"> This Month </a>
+      <a
+        class="border-indigo-500 text-indigo-600"
+        v-for="period in periods"
+        :key="period"
+      >
+        {{ period }}
+      </a>
     </nav>
   </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import { defineComponent } from '@vue/runtime-core';
+
+export default defineComponent({
+  name: 'Timeline',
+  setup() {
+    const periods = ['Today', 'This Week', 'This Month'];
+    return {
+      periods,
+    };
+  },
+});
 </script>
 
 <style></style>
