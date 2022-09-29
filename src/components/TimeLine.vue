c5e9a84d16288db3f3e6a61bf16006ba8e347cc9
Akash52 <ac8572611@gmail.com>
2022-09-29 17:21:32 +0530

    reactivity_with_ref

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -2,9 +2,13 @@
   <div className="border-b border-gray-200">
     <nav class="py-4 flex space-x-8 justify-center">
       <a
-        class="border-indigo-500 text-indigo-600"
+        class="border-indigo-500 text-indigo-600 font-normal"
         v-for="period in periods"
         :key="period"
+        :class="{
+          'border-b-2 font-medium': period === currentPeriod,
+        }"
+        @click="setPeriod(period)"
       >
         {{ period }}
       </a>
@@ -13,14 +17,22 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/runtime-core';
+import { defineComponent, ref } from 'vue';
+
+type Period = 'Today' | 'This Week' | 'This Month';
 
 export default defineComponent({
   name: 'Timeline',
   setup() {
     const periods = ['Today', 'This Week', 'This Month'];
+    const currentPeriod = ref<Period>('Today');
+    const setPeriod = (period: Period) => {
+      currentPeriod.value = period;
+    };
     return {
       periods,
+      currentPeriod,
+      setPeriod,
     };
   },
 });
