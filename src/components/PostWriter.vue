dd10d074df162f133aacfaf80bad9a5153b7748f
Akash52 <ac8572611@gmail.com>
2022-10-01 13:39:39 +0530

    testing_post_writer

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -15,6 +15,7 @@
         id="exampleFormControlInput1"
         placeholder="Content..."
         v-model="title"
+        data-test="title"
       />
     </div>
     <div
@@ -32,6 +33,7 @@
           contenteditable
           ref="contentEditable"
           @input="handleInput"
+          data-test="content"
         />
       </div>
 
@@ -51,6 +53,7 @@
       <button
         type="button"
         @click="save"
+        data-test="submit"
         class="text-white bg-yellow-700 w-32 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         Submit Post
